ee3a3c86fbed7c8d1021392431745ce172935e82
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-03-01 15:47:14 +0100

    Add files via upload

---
+++ Landsat_UC_k_means_Egypt_2023.r
@@ -0,0 +1,40 @@
+library(rgdal)
+library(raster)
+library(terra)
+library(RColorBrewer)
+library(pals)
+library(colorspace)
+library(RStoolbox)
+library(graphics)
+library(Hmisc)
+#
+# Set up working directory
+setwd("/Users/polinalemenkova/Documents/R/52_Image_Processing/K-means")
+# Importing data
+Landsat_2023 <- list.files("/Users/polinalemenkova/Documents/R/52_Image_Processing/K-means")
+# Printing the list
+list.files()
+#
+# creating SpatRaster object
+landsat <- rast(Landsat_2023)
+# check properties
+landsat
+landsatRGB <- landsat[[c(4,3,2)]]
+landsatRGB <- landsat[[c(5,4,3)]]
+landsatRGB <- landsat[[c(7,5,3)]]
+#landsatRGB <- landsat[[c(6,4,3)]]
+#landsatRGB <- landsat[[c(2,5,7)]]
+plotRGB(landsatRGB, r=1, g=2, b=3, axes=FALSE, stretch="lin")
+#
+# Running the classification
+set.seed(99)
+unC <- unsuperClass(landsatRGB, nSamples = 100, nClasses = 10, nStarts = 5)
+unC
+#
+# Creating color palette
+colors <- jet(10)
+#colors <-kovesi.diverging_rainbow_bgymr_45_85_c67(10)
+#colors <- brewer.pal(10, "Set1")
+# plotting a map
+plot(unC$map, main = "", font.main=1, cex.main = 0.85, col = colors, axes = FALSE, box = FALSE, legend = FALSE)
+legend("bottomright", inset = c(0.15, 0.0), legend=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"), fill = colors, title = "Classes", horiz = FALSE, bty = "n", text.font=1, ncol=1, y.intersp = 0.7, box.col="black")
